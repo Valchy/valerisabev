ffb62d30fa057a22fd79e319d2ad66962ddd1bf7
Valeri Sabev <valchygaming@gmail.com>
2022-05-19 19:31:17 +0200

    font optimization

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,11 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+				<link
+					/* Montserrat Font */
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100&display=swap"
+				/>
 				<Analytics />
 			</Head>
 			<body>
